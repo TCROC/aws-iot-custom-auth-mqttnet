a5ec0ff24667bd6091f96fbae1ed54ceb7c2c179
Travis Lange <travislange12@gmail.com>
2023-04-27 12:02:44 -0400

    Created simple mqttnet connection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Web;
+using MQTTnet;
+using MQTTnet.Extensions.WebSocket4Net;
+using MQTTnet.Formatter;
+using MQTTnet.Protocol;
+using MQTTnet.Diagnostics;
+
+Console.WriteLine("Running mqtt example application!");
+
+int i = 0;
+string username = args[i++];
+string password = args[i++];
+string endpoint = args[i++];
+string rootTopic = args[i++];
+string authorizer = args[i++];
+
+Console.WriteLine($"{Environment.NewLine}==================={Environment.NewLine}");
+Console.WriteLine($"Args Used:{Environment.NewLine}");
+Console.WriteLine($"{nameof(username)}: {username}");
+Console.WriteLine($"{nameof(password)}: {password}");
+Console.WriteLine($"{nameof(endpoint)}: {endpoint}");
+Console.WriteLine($"{nameof(rootTopic)}: {rootTopic}");
+Console.WriteLine($"{nameof(authorizer)}: {authorizer}");
+Console.WriteLine($"{Environment.NewLine}==================={Environment.NewLine}");
+
+var factory = new MqttFactory();
+var options = factory
+    .UseWebSocket4Net()
+    .CreateClientOptionsBuilder()
+    .WithClientId(username)
+    .WithCredentials($"username?x-amz-customauthorizer-name={HttpUtility.UrlEncode(authorizer)}", password)
+    // .WithTcpServer($"{endpoint}", 443)
+    .WithWebSocketServer($"{endpoint}/mqtt")
+    .WithWillTopic($"{rootTopic}/s/{username}")
+    .WithWillRetain(false)
+    .WithWillPayload(new byte[] { 0 })
+    .WithWillQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+    .WithTls()
+    .WithProtocolVersion(MqttProtocolVersion.V500)
+    .WithKeepAlivePeriod(TimeSpan.FromSeconds(35))
+    .Build();
+
+var logger = new MqttNetEventLogger("MqttNet");
+logger.LogMessagePublished += (obj, logArgs) => Console.WriteLine(logArgs.LogMessage.ToString());
+
+var client = factory.CreateMqttClient(logger);
+var result = await client.ConnectAsync(options);
+
+int delaySeconds = 10;
+while(client.IsConnected)
+{
+    Console.WriteLine($"Client is connected. Checking again in {delaySeconds} seconds...");
+    await Task.Delay(TimeSpan.FromSeconds(delaySeconds));
+}
