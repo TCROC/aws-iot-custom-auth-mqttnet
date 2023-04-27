8907ab33b66f9f72a138d18ba28fc0e74aa94ec2
Travis Lange <travislange12@gmail.com>
2023-04-27 13:09:27 -0400

    added logic to switch between websocket4net, dotnet, websocket, and tcp

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using MQTTnet.Extensions.WebSocket4Net;
 using MQTTnet.Formatter;
 using MQTTnet.Protocol;
 using MQTTnet.Diagnostics;
+using MQTTnet.Client;
+using System.Net.Security;
 
 Console.WriteLine("Running mqtt example application!");
 
@@ -13,6 +15,8 @@ string password = args[i++];
 string endpoint = args[i++];
 string rootTopic = args[i++];
 string authorizer = args[i++];
+string transportImplementation = args[i++]; // websocket4net | dotnet
+string transport = args[i++]; // websocket | tcp
 
 Console.WriteLine($"{Environment.NewLine}==================={Environment.NewLine}");
 Console.WriteLine($"Args Used:{Environment.NewLine}");
@@ -21,21 +25,42 @@ Console.WriteLine($"{nameof(password)}: {password}");
 Console.WriteLine($"{nameof(endpoint)}: {endpoint}");
 Console.WriteLine($"{nameof(rootTopic)}: {rootTopic}");
 Console.WriteLine($"{nameof(authorizer)}: {authorizer}");
+Console.WriteLine($"{nameof(transportImplementation)}: {transportImplementation}");
+Console.WriteLine($"{nameof(transport)}: {transport}");
 Console.WriteLine($"{Environment.NewLine}==================={Environment.NewLine}");
 
 var factory = new MqttFactory();
-var options = factory
-    .UseWebSocket4Net()
-    .CreateClientOptionsBuilder()
+
+if (transportImplementation == "websocket4net")
+{
+    factory = factory.UseWebSocket4Net();
+}
+
+var optionsBuilder = factory.CreateClientOptionsBuilder();
+
+if (transport == "tcp")
+{
+    optionsBuilder = optionsBuilder.WithTcpServer($"{endpoint}", 443);
+}
+else if (transport == "websocket")
+{
+    optionsBuilder = optionsBuilder.WithWebSocketServer($"{endpoint}/mqtt");
+}
+
+var options = optionsBuilder
     .WithClientId(username)
     .WithCredentials($"username?x-amz-customauthorizer-name={HttpUtility.UrlEncode(authorizer)}", password)
-    // .WithTcpServer($"{endpoint}", 443)
-    .WithWebSocketServer($"{endpoint}/mqtt")
     .WithWillTopic($"{rootTopic}/s/{username}")
     .WithWillRetain(false)
     .WithWillPayload(new byte[] { 0 })
     .WithWillQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
-    .WithTls()
+    .WithTls(
+        new MqttClientOptionsBuilderTlsParameters
+        {
+            UseTls = true,
+            ApplicationProtocols = new List<SslApplicationProtocol> { new("mqtt") }
+        }
+    )
     .WithProtocolVersion(MqttProtocolVersion.V500)
     .WithKeepAlivePeriod(TimeSpan.FromSeconds(35))
     .Build();
@@ -47,7 +72,7 @@ var client = factory.CreateMqttClient(logger);
 var result = await client.ConnectAsync(options);
 
 int delaySeconds = 10;
-while(client.IsConnected)
+while (client.IsConnected)
 {
     Console.WriteLine($"Client is connected. Checking again in {delaySeconds} seconds...");
     await Task.Delay(TimeSpan.FromSeconds(delaySeconds));
