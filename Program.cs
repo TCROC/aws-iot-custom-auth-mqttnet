133ca053b9c70a7f3843f568582d5e4554d7a300
Travis Lange <travislange12@gmail.com>
2023-05-06 18:31:28 -0400

    added support for both certificates and lambda functions

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using MQTTnet.Protocol;
 using MQTTnet.Diagnostics;
 using MQTTnet.Client;
 using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 
 Console.WriteLine("Running mqtt example application!");
 
@@ -14,7 +15,8 @@ string username = args[i++];
 string password = args[i++];
 string endpoint = args[i++];
 string rootTopic = args[i++];
-string authorizer = args[i++];
+string authType = args[i++]; // lambda | cert
+string authorizer = args[i++]; // iot authorizer | cert paths (ca,crt,key)
 string transportImplementation = args[i++]; // websocket4net | dotnet
 string transport = args[i++]; // websocket | tcp
 
@@ -24,6 +26,7 @@ Console.WriteLine($"{nameof(username)}:                  {username}");
 Console.WriteLine($"{nameof(password)}:                  {password}");
 Console.WriteLine($"{nameof(endpoint)}:                  {endpoint}");
 Console.WriteLine($"{nameof(rootTopic)}:                 {rootTopic}");
+Console.WriteLine($"{nameof(authType)}:                  {authType}");
 Console.WriteLine($"{nameof(authorizer)}:                {authorizer}");
 Console.WriteLine($"{nameof(transportImplementation)}:   {transportImplementation}");
 Console.WriteLine($"{nameof(transport)}:                 {transport}");
@@ -47,20 +50,48 @@ else if (transport == "websocket")
     optionsBuilder = optionsBuilder.WithWebSocketServer($"{endpoint}/mqtt");
 }
 
+if (authType == "lambda")
+{
+    optionsBuilder = optionsBuilder
+        .WithCredentials($"username?x-amz-customauthorizer-name={HttpUtility.UrlEncode(authorizer)}", password)
+        .WithTls(
+            new MqttClientOptionsBuilderTlsParameters
+            {
+                UseTls = true,
+                ApplicationProtocols = new List<SslApplicationProtocol> { new("mqtt") },
+            }
+        );
+}
+else if (authType == "cert")
+{
+    var caCrtKey = authorizer.Split(",");
+    var caCert = X509Certificate2.CreateFromPem(File.ReadAllText(caCrtKey[0]));
+    var cert = X509Certificate2.CreateFromPemFile(caCrtKey[1], caCrtKey[2]);
+    optionsBuilder = optionsBuilder
+        .WithTls(
+            new MqttClientOptionsBuilderTlsParameters
+            {
+                UseTls = true,
+                ApplicationProtocols = new List<SslApplicationProtocol> { new("mqtt") },
+                Certificates = new X509Certificate[] 
+                {
+                    caCert,
+                    cert
+                },
+            }
+        );
+}
+else
+{
+    throw new Exception($"Invalid {nameof(authType)}: {authType}");
+}
+
 var options = optionsBuilder
     .WithClientId(username)
-    .WithCredentials($"username?x-amz-customauthorizer-name={HttpUtility.UrlEncode(authorizer)}", password)
     .WithWillTopic($"{rootTopic}/s/{username}")
     .WithWillRetain(false)
     .WithWillPayload(new byte[] { 0 })
     .WithWillQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
-    .WithTls(
-        new MqttClientOptionsBuilderTlsParameters
-        {
-            UseTls = true,
-            ApplicationProtocols = new List<SslApplicationProtocol> { new("mqtt") }
-        }
-    )
     .WithProtocolVersion(MqttProtocolVersion.V500)
     .WithKeepAlivePeriod(TimeSpan.FromSeconds(35))
     .Build();
